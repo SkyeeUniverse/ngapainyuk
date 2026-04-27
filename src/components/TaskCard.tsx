2861e34b1fdb4d221edee3d6a16a58d71785430b
Fajrin <me.fajrinn@gmail.com>
2026-04-27 21:11:13 +0800

    Implement Drag and Drop

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,5 @@
 import { ITask } from "@/types/Task";
+import { useDraggable } from "@dnd-kit/core";
 
 interface TaskCardProps {
   task: ITask;
@@ -7,10 +8,22 @@ interface TaskCardProps {
 
 const TaskCard = (props: TaskCardProps) => {
   const { task, key } = props;
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: task.id,
+  });
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
   return (
     <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
       key={key}
       className="cursor-grab rounded-lg bg-neutral-50 p-4 shadow-sm hover:shadow-md"
+      style={style}
     >
       <h3 className="font-medium text-neutral-700">{task.title}</h3>
       <p className="mt-2 text-sm text-neutral-500">{task.description}</p>
